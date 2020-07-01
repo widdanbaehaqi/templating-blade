35f7f2ce967332aa0c0dfdc0ea2303577920e599
widdanbaehaqi <widdanbaehaqi@mhs.stmik-yadika.ac.id>
2020-07-02 00:12:59 +0700

    mod:routes add /data-tables. add:asset,template,view home & datatable

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
+});
+Route::get('/data-tables', function () {
+    return view('datatables');
 });
